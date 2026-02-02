4924bcc768190dc74b8cfb93f784a26b77c2233e
AMirster <139302115+uniii11223@users.noreply.github.com>
2026-02-02 21:57:38 +0800

    Add files via upload

---
+++ src/client/kotlin/etherealscript/modid/EtherealScriptDataGenerator.kt
@@ -0,0 +1,9 @@
+package etherealscript.modid
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object EtherealScriptDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
